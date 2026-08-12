a57c4cff916e2a3725b176f70a0edfe40160b959
CheSubhro <mitsubhra@gmail.com>
2026-08-12 17:28:08 +0530

    add user role system

---
+++ constants/roles.js
@@ -0,0 +1,8 @@
+
+export const ROLES = {
+    ADMIN: "admin",
+    EDITOR: "editor",
+    AUTHOR: "author",
+};
+
+export const ROLE_VALUES = Object.values(ROLES);
--- models/User.js
+++ models/User.js
@@ -1,45 +1,46 @@
 
 import mongoose from "mongoose";
+import { ROLE_VALUES, ROLES } from "@/constants/roles";
 
 const userSchema = new mongoose.Schema(
-    {
-        name: {
-        type: String,
-        required: [true, "Name is required"],
-        trim: true,
-        minlength: 2,
-        maxlength: 100,
-        },
-
-        email: {
-        type: String,
-        required: [true, "Email is required"],
-        unique: true,
-        lowercase: true,
-        trim: true,
-        },
-
-        password: {
-        type: String,
-        required: [true, "Password is required"],
-        minlength: 6,
-        select: false,
-        },
-
-        role: {
-        type: String,
-        enum: ["admin", "editor", "author"],
-        default: "author",
-        },
-
-        isActive: {
-        type: Boolean,
-        default: true,
-        },
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+      minlength: 2,
+      maxlength: 100,
     },
-    {
-        timestamps: true,
+
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
     },
+
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: 6,
+      select: false,
+    },
+
+    role: {
+      type: String,
+      enum: ROLE_VALUES,
+      default: ROLES.AUTHOR,
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
 );
 
 const User = mongoose.models.User || mongoose.model("User", userSchema);
