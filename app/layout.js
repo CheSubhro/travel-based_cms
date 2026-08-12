5614608b25309119e4e8bbdc140d4a6c30c1ad48
CheSubhro <mitsubhra@gmail.com>
2026-08-12 14:41:24 +0530

    setup Prettier

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,14 @@
-
 export const metadata = {
-	title: "Travel CMS",
-	description: "Travel Content Management System",
+  title: "Travel CMS",
+  description: "Travel Content Management System",
 };
 
 import "./globals.css";
 
 export default function RootLayout({ children }) {
-	return (
-		<html lang="en">
-			<body>{children}</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
 }
--- app/page.js
+++ app/page.js
@@ -1,20 +1,17 @@
-
 export default function Home() {
-  	return (
-		<main className="min-h-screen bg-gray-100 flex items-center justify-center">
-		<div className="bg-white p-10 rounded-2xl shadow-lg text-center">
-			<h1 className="text-4xl font-bold text-blue-600 mb-4">
-			Travel CMS
-			</h1>
+  return (
+    <main className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <div className="bg-white p-10 rounded-2xl shadow-lg text-center">
+        <h1 className="text-4xl font-bold text-blue-600 mb-4">Travel CMS</h1>
 
-			<p className="text-gray-600 mb-6">
-			Tailwind CSS is working successfully!
-			</p>
+        <p className="text-gray-600 mb-6">
+          Tailwind CSS is working successfully!
+        </p>
 
-			<button className="bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700">
-			Explore Travel
-			</button>
-		</div>
-		</main>
-	);
+        <button className="bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700">
+          Explore Travel
+        </button>
+      </div>
+    </main>
+  );
 }
