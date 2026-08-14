fe965a0a4461db009ac3750461f2b370823c1109
CheSubhro <mitsubhra@gmail.com>
2026-08-14 20:29:01 +0530

    Some changes for folder and file structure for admin panel dashboard cms sidebar

---
+++ app/admin/(dashboard)/layout.js
@@ -0,0 +1,14 @@
+
+import Sidebar from "../components/Sidebar";
+
+export default function DashboardLayout({ children }) {
+    return (
+        <div className="min-h-screen bg-gray-50">
+            <Sidebar />
+
+            <main className="ml-64 min-h-screen">
+                {children}
+            </main>
+        </div>
+    );
+}
--- app/admin/layout.js
+++ app/admin/layout.js
@@ -1,11 +1,4 @@
-import Sidebar from "./components/Sidebar";
 
 export default function AdminLayout({ children }) {
-    return (
-        <div className="min-h-screen bg-gray-50">
-            <Sidebar />
-
-            <main className="ml-64 min-h-screen">{children}</main>
-        </div>
-    );
+    return children;
 }
