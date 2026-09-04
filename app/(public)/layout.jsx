7ea8408f782f660cdafb70fef04915f0222cee6f
CheSubhro <mitsubhra@gmail.com>
2026-09-04 16:10:32 +0530

    Some folder structure change

---
+++ app/(public)/layout.jsx
@@ -0,0 +1,15 @@
+
+
+import Navbar from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
+
+export default function PublicLayout({ children }) {
+    return (
+        <>
+            <Navbar />
+            {children}
+            <Footer />
+        </>
+    );
+}
+
--- app/layout.js
+++ app/layout.js
@@ -4,8 +4,7 @@ import "./globals.css";
 
 import siteConfig from "@/config/site";
 
-import Navbar from "@/components/layout/Navbar";
-import Footer from "@/components/layout/Footer";
+
 
 export const metadata = {
     metadataBase: new URL(
@@ -61,11 +60,9 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body>
-                <Navbar />
 
                 {children}
 
-                <Footer />
             </body>
         </html>
     );
